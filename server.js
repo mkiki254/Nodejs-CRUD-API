f66457694fe43214c84fe93f4d3f648131ec456d
mkiki254 <kariukicharles440@gmail.com>
2023-09-01 13:49:21 +0300

    Worked on the get request for all movies

--- server.js
+++ server.js
@@ -4,18 +4,41 @@ const http = require("http");
 // To use the port from the env file
 require("dotenv").config();
 
+// Importing all the four methods in the server.js
+const getReq = require("./methods/get-request");
+const postReq = require("./methods/post-request");
+const putReq = require("./methods/put-request");
+const deleteReq = require("./methods/delete-request");
+
+// Importing the movies data
+let movies = require("./data/movies.json");
+
 // The port where the server will listen
 const PORT = process.env.PORT || 5001;
 
 // Creating a HTTP server
 const server = http.createServer((req, res) => {
-    // Throwing a simple response
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.write(JSON.stringify({message: "Hello Charles, How is Nodejs API Project"}));
-    res.end();
-    // When you search: localhost:5000 on the browser, you see the above message
-    // It is a get request
+    req.movies = movies;
+
+    switch(req.method) {
+        case "GET": 
+            getReq(req, res);
+            break;
+        case "POST":
+            postReq(req, res);
+            break;
+        case "PUT":
+            putReq(req, res);
+            break;
+        case "DELETE":
+            deleteReq(req, res);
+            break;
+        default:
+            res.statusCode = 404;
+            res.setHeader("Content-Type", "application/json");
+            res.write(JSON.stringify({title: "Not Found", message: "Route Not Found"}));
+            res.end();
+    }
 });
 
 // We need to listen for this server on a port
